dbe5f843dc3df54312c79bccaa9322e7c32d1114
voidash <ashish.thapa477@gmail.com>
2022-02-21 21:30:40 +0545

    complete IPfinder

--- showip.c
+++ showip.c
@@ -33,9 +33,21 @@ int main(int argc, char* argv[]) {
 		char *ipver;
 
 		if(p->ai_family == AF_INET) {
-
+			struct sockaddr_in *ipv4 = (struct sockaddr_in *) p->ai_addr;
+			addr = &(ipv4 -> sin_addr);
+			ipver = "IPv4";
+		}else {
+			struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) p->ai_addr;
+			addr = &(ipv6->sin6_addr);
+			ipver = "IPv6";
 		}
+
+		inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+		printf(" %s: %s\n",ipver, ipstr);
 	}
+	freeaddrinfo(res);
+
+	return 0;
 	
 
 }
